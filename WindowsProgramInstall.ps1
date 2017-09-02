5b7a78f7c0268455792c51aeb123f6d6eec77781
Lazar Lazarevic <lazarlazarevic@msn.com>
2017-09-02 16:48:10 +0200

    updated program list

--- WindowsProgramInstall.ps1
+++ WindowsProgramInstall.ps1
@@ -6,9 +6,11 @@ choco feature enable -n allowGlobalConfirmation
 
 #programs
 choco install googlechrome
+choco install bsplayer
 choco install anti-beacon
 choco install utorrent
 choco install time4popcorn
 choco install git
+choco install notepadplusplus.install
 choco install visualstudiocode
 choco install visualstudio2017community
